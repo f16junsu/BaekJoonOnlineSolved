bb366fea4324ff0574b1207af03a17a04b4677b1
KimMinjae <f16junsu@snu.ac.kr>
2021-11-15 20:14:53 +0900

    Succesful

--- LV9/Q9-4.py
+++ LV9/Q9-4.py
@@ -1,8 +1,14 @@
+from math import sqrt
+
 M, N = map(int, input().split())
-Ls = [n for n in range(2, N + 1) if n % 2 != 0]
+Ls = [2] + [n for n in range(2, N + 1) if n % 2 != 0]
 ls = []
 while Ls:
-    ls.append(Ls.pop(0))
-    Ls = list(filter(lambda x : x % ls[-1] != 0, Ls))
-    if ls[-1] >= M:
-        print(ls[-1])
+    num = Ls.pop(0)
+    if (num >= sqrt(N)):
+        ls = ls + Ls
+        break
+    ls.append(num)
+    Ls = list(filter(lambda x : x % num != 0, Ls))
+for i in list(filter(lambda x : x >= M, ls)):
+    print(i)
