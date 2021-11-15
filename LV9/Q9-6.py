8ecc87c3cfb0ae27fab0d89af04a4c9f18fe906e
KimMinjae <f16junsu@snu.ac.kr>
2021-11-15 22:44:59 +0900

    Successful

--- LV9/Q9-6.py
+++ LV9/Q9-6.py
@@ -1,25 +1,16 @@
 def gold_partitions(num):
     table = [False, False] + [True] * (num - 1)
-    primes = []
-    partitions = []
+    partitions = ()
     for i in range(2, int(num**.5) + 1):
         if table[i]:
             for j in range(2 * i, num + 1, i):
                 table[j] = False
-    for i in range(num + 1):
-        if table[i]:
-            primes.append(i)
-    partitions = []
-    for n in primes:
-        if n > num // 2:
-            break
-        if primes.count(num - n):
-            partitions.append((n, num - n))
+    for i in range(2, num // 2 + 1):
+        if table[i] and table[num - i]:
+            partitions = (i, num - i)
     return partitions
 
 T = int(input())
 for i in range(T):
     num = int(input())
-    partitions = gold_partitions(num)
-    m, n = partitions.pop()
-    print(m, n)
+    print(*gold_partitions(num))
