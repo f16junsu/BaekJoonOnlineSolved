9576b969d8aa46d7730224be1b3096354ed3d9a8
KimMinjae <f16junsu@snu.ac.kr>
2021-11-15 22:33:21 +0900

    Time overlapse2

--- LV9/Q9-6.py
+++ LV9/Q9-6.py
@@ -1,29 +1,25 @@
 def gold_partitions(num):
-    ls = [False, False] + [True] * (num - 1)
-    for i in range(2, int(num**.5) + 1):
-        if ls[i]:
-            for j in range(2 * i, num + 1, i):
-                ls[j] = False
+    table = [False, False] + [True] * (num - 1)
     primes = []
+    partitions = []
+    for i in range(2, int(num**.5) + 1):
+        if table[i]:
+            for j in range(2 * i, num + 1, i):
+                table[j] = False
     for i in range(num + 1):
-        if ls[i]:
+        if table[i]:
             primes.append(i)
     partitions = []
-    func = lambda x, y: (x, y) if x <= y else (y, x)
     for n in primes:
+        if n > num // 2:
+            break
         if primes.count(num - n):
-            t = func(n, num - n)
-            if t in partitions:
-                continue
-            else:
-                partitions.append(t)
+            partitions.append((n, num - n))
     return partitions
 
 T = int(input())
 for i in range(T):
     num = int(input())
     partitions = gold_partitions(num)
-    m, n = partitions[-1]
+    m, n = partitions.pop()
     print(m, n)
-    
-    #이놈의 시간초과 개빡치네
