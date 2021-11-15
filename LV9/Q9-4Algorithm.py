436128931b9b174a95f2e2e6b496015b8e0ccc80
KimMinjae <f16junsu@snu.ac.kr>
2021-11-15 20:41:59 +0900

    Comopleted

--- LV9/Q9-4Algorithm.py
+++ LV9/Q9-4Algorithm.py
@@ -2,7 +2,7 @@ M, N = map(int, input().split())
 ls = [False, False] + [True] * (N - 1)
 for i in range(2, int(N**.5) + 1):
     if ls[i]:
-        for j in range(2 * i, N, i):
+        for j in range(2 * i, N + 1, i):
             ls[j] = False
 for i in range(N + 1):
     if ls[i] and i >= M:
